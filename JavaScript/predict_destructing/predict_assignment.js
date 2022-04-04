0d9b5803a3e45f23236ee68f5a6a543b82aee43e
xrodbeex <rodney.bautista92@outlook.com>
2022-04-04 08:19:44 -0700

    prediction assignment

---
+++ JavaScript/predict_destructing/predict_assignment.js
@@ -0,0 +1,63 @@
+// PROBLEM 1.
+const cars = ['Tesla', 'Mercedes', 'Honda']
+const [ randomCar ] = cars
+const [ ,otherRandomCar ] = cars
+//Predict the output
+console.log(randomCar)
+console.log(otherRandomCar)
+// randomCar = Tesla
+//otherRandomCar = Mercedes
+
+
+// Problem 2
+const employee = {
+    name: 'Elon',
+    age: 47,
+    company: 'Tesla'
+}
+const { name: otherName } = employee;
+//Predict the output
+console.log(name);
+console.log(otherName);
+//name is not defined
+//otherName is Elon
+
+//Problem 3
+const employee = {
+    name: 'Elon',
+    age: 47,
+    company: 'Tesla'
+}
+const { name: otherName } = employee;
+//Predict the output
+console.log(name);
+console.log(otherName);
+
+
+//Problem 4
+
+const numbers = [8, 2, 3, 5, 6, 1, 67, 12, 2];
+const [,first] = numbers;
+const [,,,second] = numbers;
+const [,,,,,,,,third] = numbers;
+//Predict the output
+console.log(first == second);
+console.log(first == third);
+//2,5,2
+//FALSE, TRUE
+
+//Problem 5
+const lastTest = {
+    key: 'value',
+    secondKey: [1, 5, 1, 8, 3, 3]
+}
+const { key } = lastTest;
+const { secondKey } = lastTest;
+const [ ,willThisWork] = secondKey;
+//Predict the output
+console.log(key);
+console.log(secondKey);
+console.log(secondKey[0]);
+console.log(willThisWork);
+
+//value, [1,5,1,8,3,3], 1, 5
