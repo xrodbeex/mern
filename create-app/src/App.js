72500b61355c56951c07247a1ed4151622ddb97d
xrodbeex <rodney.bautista92@outlook.com>
2022-04-07 17:40:13 -0700

    my first react app

---
+++ create-app/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Dojo</h1>
+      <h3>Things I need to do:</h3>
+      <ul>
+        <li>Learn React</li>
+        <li>Get a Great Job</li>
+        <li>finish this school</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
