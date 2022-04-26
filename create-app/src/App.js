77af380c99c01cbd62b727e4a56d0825543ec1aa
xrodbeex <rodney.bautista92@outlook.com>
2022-04-26 09:48:34 -0700

    pokemon

--- create-app/src/App.js
+++ create-app/src/App.js
@@ -5,7 +5,7 @@ function App() {
     const [pokemon, setPokemon] = useState([]);
 
     useEffect(() => {
-      fetch('https://pokeapi.co/api/v2/pokemon')
+      fetch('https://pokeapi.co/api/v2/pokemon?limit=807')
         .then(response => response.json())
         .then(response => setPokemon(response.results))
     }, []);
