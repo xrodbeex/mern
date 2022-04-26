890f22a91148257bb1907d1d7f11d2f265c57c83
xrodbeex <rodney.bautista92@outlook.com>
2022-04-25 17:57:55 -0700

    pokemon api

--- create-app/src/App.js
+++ create-app/src/App.js
@@ -1,7 +1,35 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
+function App() {
+    const [pokemon, setPokemon] = useState([]);
 
+    useEffect(() => {
+      fetch('https://pokeapi.co/api/v2/pokemon')
+        .then(response => response.json())
+        .then(response => setPokemon(response.results))
+    }, []);
+
+    return (
+      <div>
+        {pokemon.length > 0 && pokemon.map((poke, index) => {
+          return (<div key={index}>{poke.name}</div>)
+        })}
+      </div>
+    );
+}
+
+
+
+
+
+
+
+
+
+
+
+/*
 function App() {
   const [newTodo, setNewTodo ] = useState("");
   const [todos, setTodos] = useState([]);
@@ -92,7 +120,7 @@ function App() {
     </div>
   );
 }
-
+*/
 
 
 
